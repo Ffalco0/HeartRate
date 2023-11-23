13ea49112fb085d49a75a4ab03c8c5abd022578d
Ffalco0 <falcofabio707@gmail.com>
2023-11-23 17:34:15 +0100

    fisrt commit

---
+++ HeartRate/HeartRate/HeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateApp.swift
+//  HeartRate
+//
+//  Created by Fabio Falco on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
